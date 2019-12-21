3aff2d9f9b48c35f3013616c60715662ae53f7d6
Lora Johns <lora.r.johns@gmail.com>
2019-12-21 13:52:57 -0500

    commented the shiny script to explain its pointlessness

--- shiny.py
+++ shiny.py
@@ -1,5 +1,9 @@
 from halo import Halo
-
+'''
+a really dumb script
+that wastes some time
+to show how the cool spinner works
+'''
 spinner = Halo(text='ooo shiny')
 spinner.start()
 nums = [i**j for i in range(1000) for j in range(1000)]
