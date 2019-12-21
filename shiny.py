769396cc06be85fb9cb69a5fc4e5a40cee3a5541
Lora Johns <lora.r.johns@gmail.com>
2019-12-21 13:55:43 -0500

    Changed shiny output to reflect inanity.

--- shiny.py
+++ shiny.py
@@ -8,6 +8,6 @@ spinner = Halo(text='ooo shiny')
 spinner.start()
 nums = [i**j for i in range(1000) for j in range(1000)]
 
-spinner.succeed(text=f'Printing {len(nums)} articles. ===> Done')
+spinner.succeed(text=f'All done! Calculated {len(nums)} numbers.')
 
 __name__ == "__main__"
